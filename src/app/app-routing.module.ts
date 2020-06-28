417fc611533dff7939af0ac58fdfb55419118dcc
Landry Trebon <landry.trebon@gmail.com>
2020-06-28 12:52:19 +0200

    add menu links and about module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: 'npod',
-    loadChildren: () => import("./npod/npod.module").then(m => m.NpodModule),
+    loadChildren: () => import("./npod/npod.module").then(m => m.NpodModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import("./about/about.module").then(m => m.AboutModule)
   },
   {
     path: '**',
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CursorComponent } from './components/cursor/cursor.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [CursorComponent, HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [CursorComponent, HeaderComponent]
 })
 export class SharedModule { }
